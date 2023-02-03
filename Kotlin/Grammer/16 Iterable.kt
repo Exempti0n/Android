6819a5d860652388d518e6a8cc84b04cc6513f56
seungjun oh <sj990927@gmail.com>
2023-02-03 19:13:29 +0900

    Iterable-1

--- Kotlin/Grammer/16 Iterable.kt
+++ Kotlin/Grammer/16 Iterable.kt
@@ -22,6 +22,55 @@ fun main(args: Array<String>) {
 //        문자열 + Int(정수) = 문자열
 //        문자열 + 아무거나 = 문자열
     }
+    println()
 
+//   반복하는 방법(3)
+    a.forEach {
+        println(it)
+    }
+    println()
 
+//    반복하는 방법(4)
+    a.forEach { item ->
+        println(item)
+    }
+    println()
+//    반복하는 방법(5)
+    a.forEachIndexed { index, item ->
+        println("index : " + index + " value : " + item)
+    }
+    println()
+//    반복하는 방법(6)
+    println(a.size)
+    println()
+    for (i in 0 until a.size) {
+//        until은 마지막을 포함하지 않는다
+//        0부터 8까지이다
+        println(a.get(i))
+    }
+    println()
+
+//    반복하는 방법(7)
+    for (i in 0 until a.size step (2)) {
+        println(a.get(i))
+    }
+    println()
+
+//    반복하는 방법(8)
+    for (i in a.size - 1 downTo (0)) {
+//        8부터 0까지 반복
+        println(a.get(i))
+    }
+    println()
+
+//    반복하는 방법(9)
+    for (i in a.size - 1 downTo (0) step (2)) {
+        println(a.get(i))
+    }
+    println()
+
+//    반복하는 방법(10)
+    for (i in 0..a.size) {
+        println(i)
+    }
 }
