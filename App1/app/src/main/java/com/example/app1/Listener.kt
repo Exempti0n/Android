a3316f17fbe0d7faf10e3c7ba86472c2fe490a83
seungjun oh <sj990927@gmail.com>
2023-02-21 23:58:29 +0900

    How to control view

--- App1/app/src/main/java/com/example/app1/Listener.kt
+++ App1/app/src/main/java/com/example/app1/Listener.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.app1.databinding.ActivityListenerBinding
 
 class Listener : AppCompatActivity() {
+    var number = 10
     private val binding by lazy { ActivityListenerBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,11 @@ class Listener : AppCompatActivity() {
 //        1 -> 람다
         binding.hello.setOnClickListener {
             Log.d("click", "Click!")
+            binding.hello.setText("안녕하세요")
+            binding.image.setImageResource(R.drawable.messi)
+            number += 10
+            Log.d("number", "" + number)// string + 다른 자료형 = 스트링타입
+
         }
 ////        2 -> 익명 함수 방식
 //        binding.hello.setOnClickListener(object : View.OnClickListener {
@@ -36,6 +42,14 @@ class Listener : AppCompatActivity() {
 //            }
 //        }
 //        binding.hello.setOnClickListener(click)
+//        리스너 단 것은 제일 마지막 것이 적용된다.
+
+//        뷰를 조작하는 함수들
+//       1> setText
+//       2> setImageResource
+    //    xml에 그리는 뷰는 정적이다. 동적으로 바꾸고 싶을 때는 xml을 액티비티로 가져와서
+//        바꾼다.
+//
 
 
     }
