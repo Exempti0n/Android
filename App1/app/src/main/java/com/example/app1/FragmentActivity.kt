04f56445782a82a06263f3e747f573ac649efbbc
seungjun oh <sj990927@gmail.com>
2023-02-27 23:02:36 +0900

    Fragment 3

--- App1/app/src/main/java/com/example/app1/FragmentActivity.kt
+++ App1/app/src/main/java/com/example/app1/FragmentActivity.kt
@@ -3,6 +3,7 @@ package com.example.app1
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import com.example.app1.databinding.ActivityFragmentBinding
 
 class FragmentActivity : AppCompatActivity() {
@@ -13,6 +14,21 @@ class FragmentActivity : AppCompatActivity() {
 
         Log.d("life_cycle", "onCreate")
 
+        binding.button.setOnClickListener {
+            //프라그먼트를 동적으로 작동하는 방법
+            val fragmentOne : FragmentOne = FragmentOne()
+            val fragmentManager :FragmentManager = supportFragmentManager
+
+            // Transaction
+            // 작업의 단위 -> 시작과 끝이 있다
+            val fragmentTransaction = fragmentManager.beginTransaction() //시작
+            fragmentTransaction.replace(R.id.container, fragmentOne) // 할 일
+            fragmentTransaction.commit() //끝
+            //끝을 내는 방법
+            // commit       -> 시간 될 때 해 (좀 더 안정적)
+            // commitnow    -> 지금 당장 해
+
+        }
 
     }
 
